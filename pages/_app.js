9f8f37906c56c3741561c32c21d4561b13c9f3cb
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-09-07 15:40:57 -0700

    config changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ApolloProvider } from '@apollo/client';
+import client from '../apollo-client';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+    <ChakraProvider>  
+        <Box w='80%' mx='auto' p='5' mb='24'>
+          <Component {...pageProps} />
+        </Box>
+    </ChakraProvider>
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
