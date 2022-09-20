96cf7f2c6d6480a1d8830f42300963c10084c04b
mayanhavoc <mayanhavoc@gmail.com>
2022-09-19 17:30:22 -0700

    add off screen drawer for navigation

--- components/Nav.js
+++ components/Nav.js
@@ -1,20 +1,53 @@
-import Link from 'next/link'
-import { Flex, Spacer } from '@chakra-ui/react'
+import React from 'react';
+import Link from 'next/link';
+import { useDisclosure } from '@chakra-ui/react';
+import { Button, Flex, Spacer } from '@chakra-ui/react'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
+
 
 export default function Nav() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
   return (
-      <Flex direction="row" w="100%" p="5" bg="gray.300">
-        <Spacer />
-        <Link href="/">Home</Link>
-        <Spacer />
-        <Link href="/about">About</Link>
-        <Spacer />
-        <Link href="/index">Blog</Link>
-        <Spacer />
-        <Link href="/work">Work</Link>
-        <Spacer />
-        <Link href="/contact">Contact</Link>
-        <Spacer />
-      </Flex>
+    <>
+      <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerBody>
+          <Flex direction="column" w="100%" p="5" bg="gray.300">
+            <Spacer />
+            <Link href="/">Home</Link>
+            <Spacer />
+            <Link href="/about">About</Link>
+            <Spacer />
+            <Link href="/index">Blog</Link>
+            <Spacer />
+            <Link href="/work">Work</Link>
+            <Spacer />
+            <Link href="/contact">Contact</Link>
+            <Spacer />
+          </Flex>
+        </DrawerBody>
+      </DrawerContent>
+      </Drawer>
+    </>
   )
 }
